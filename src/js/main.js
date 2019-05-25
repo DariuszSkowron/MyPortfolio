fdfe8807ce7fa2a68540d7c3ad4d288c73add04f
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-05-25 19:11:54 +0200

    added media queries and menu overlay responsive

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,14 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('nav-item');
+const navItems = document.querySelectorAll('.nav-item');
 
 /*Set initial state of menu*/
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
 
-function toggleMenu()
-{
+function toggleMenu() {
     if (!showMenu) {
         menuBtn.classList.add('close');
         menu.classList.add('show');
@@ -19,24 +18,17 @@ function toggleMenu()
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
 
-/*Set Menu State*/
+        /*Set Menu State*/
         showMenu = true;
     } else {
-            menuBtn.classList.remove('close');
-            menu.classList.remove('show');
-            menuNav.classList.remove('show');
-            menuBranding.classList.remove('show');
-            navItems.forEach(item => item.classList.remove('show'));
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
 
-            /*Set Menu State*/
-            showMenu = true;
+        /*Set Menu State*/
+        showMenu = false;
     }
-    {
-
-    }
-}
-
-{
-
 }
 
