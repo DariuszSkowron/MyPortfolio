dbea08afc2c9e8b8516928e5467e50992749c605
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-09-28 23:06:01 +0200

    hamburger menu changes

--- www/js/main.js
+++ www/js/main.js
@@ -5,11 +5,15 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
+const links = document.querySelectorAll('.menu-nav li a');
 
 /*Set initial state of menu*/
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
+for (var i = 0; i < links.length; i++){
+    links[i].addEventListener('click', toggleMenu);
+}
 
 function toggleMenu() {
     if (!showMenu) {
@@ -32,4 +36,5 @@ function toggleMenu() {
         showMenu = false;
     }
 }
+
 
