1935ad927ca4528f5192bbbf8afb1c505976df02
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-11-19 00:11:52 +0100

    cosmetic changes

--- www/js/generateResume.js
+++ www/js/generateResume.js
@@ -1,15 +1,16 @@
-function generatePdf() {
+function generatePdf(pp) {
     // Get the element.
     var element = document.getElementById('cv');
+
 
     // Generate the PDF.
     html2pdf().from(element).set({
         margin: [-10,0,-100,0],
-        filename: 'DariuszSkowron-CV-ENG.pdf',
+        filename: 'Dariusz_Skowron_CV_' + pp + '.pdf',
         html2canvas: {windowWidth: 2000, scale: 2 },
-        // jsPDF: {orientation: 'p', unit: 'mm', format: [350, 250], compressPDF: true}
         jsPDF: {orientation: 'p', unit: 'mm', format: 'a4', compressPDF: false}
     }).save();
 }
+
 
 
